226a29fdced488608c755609816fa857e90183ef
Aliya <aliyafthh@gmail.com>
2025-08-04 16:15:16 +0800

    [bloc-demo] - create simple counter using bloc

---
+++ bloc_demo/lib/counter/counter_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<int> {
+  CounterCubit(super.initialState);
+
+  void increment() => emit(state + 1);
+  void decrement() => emit(state - 1);
+
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+    print(change);
+  }
+}
